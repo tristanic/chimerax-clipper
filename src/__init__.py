25c50e7d0172634d83695529c0f261c9511f7c03
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2019-11-18 17:14:09 +0000

    Properly implemented BundleAPI get_class()
    Needs to specify name-to-class for all State subclasses - even those for 
    which SESSION_SAVE is False.

--- src/__init__.py
+++ src/__init__.py
@@ -138,6 +138,38 @@ class _ClipperBundle(BundleAPI):
     # any.
     @staticmethod
     def get_class(class_name):
-        return None
+        from .symmetry import Symmetry_Manager, AtomicSymmetryModel
+        from .graphics.hkl_plot import HKL_Plot_3D
+        from .maps import (
+            XmapSet, Map_Mgr, XmapHandler_Static,
+            XmapHandler_Live, NXmapSet, NXmapHandler
+            )
+        from .maps.map_handler_base import FastVolumeSurface
+        from .maps.mask_handler import VolumeMask, ZoneMask
+        from .clipper_mtz import (
+            ReflectionDataContainer, ReflectionData_Node, ReflectionData,
+            ReflectionData_FreeFlags, ReflectionData_Exp, ReflectionData_Calc,
+        )
+        ct = {
+            'Symmetry_Manager':     Symmetry_Manager,
+            'AtomicSymmetryModel':  AtomicSymmetryModel,
+            'HKL_Plot_3D':          HKL_Plot_3D,
+            'XmapSet':              XmapSet,
+            'Map_Mgr':              Map_Mgr,
+            'XmapHandler_Static':   XmapHandler_Static,
+            'XmapHandler_Live':     XmapHandler_Live,
+            'NXmapSet':             NXmapSet,
+            'NXmapHandler':         NXmapHandler,
+            'FastVolumeSurface':    FastVolumeSurface,
+            'VolumeMask':           VolumeMask,
+            'ZoneMask':             ZoneMask,
+            'ReflectionDataContainer': ReflectionDataContainer,
+            'ReflectionData_Node':  ReflectionData_Node,
+            'ReflectionData':       ReflectionData,
+            'ReflectionData_FreeFlags': ReflectionData_FreeFlags,
+            'ReflectionData_Exp':   ReflectionData_Exp,
+            'ReflectionData_Calc':  ReflectionData_Calc
+        }
+        return ct.get(class_name)
 
 bundle_api = _ClipperBundle()
